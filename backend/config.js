b8daf97876ce593689c0eb34d06811d29bcf90d3
Austin Wang <wangaus1997@gmail.com>
2024-02-05 13:41:28 -0800

    First

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 8080;
+
+export const mongoDBURL =
+  'mongodb+srv://book:book@books-store-mern.chkv3jl.mongodb.net/book-collection?retryWrites=true&w=majority'
---
+++ fronted/tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     [
+//       "./index.html",
+//       "./src/**/*.{js,ts,jsx,tsx}",
+//     ],
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
